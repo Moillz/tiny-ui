6bf66b420e3e9422e2dcbf183904b85aa31f4577
muliqin <muliqin@bigo.sg>
2021-04-14 15:20:09 +0800

    feat: 开发环境搭建成功

--- src/index.js
+++ src/index.js
@@ -1,20 +1,6 @@
-import _ from 'lodash';
-import printMe from './print.js'
+import Vue from 'vue';
+import App from './App.vue'
 
- function component() {
-   const element = document.createElement('div');
-   const btn = document.createElement('button');
-
-  // lodash（目前通过一个 script 引入）对于执行这一行是必需的
-  // lodash 在当前 script 中使用 import 引入
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-   btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
-
-   return element;
- }
-
- document.body.appendChild(component());
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
